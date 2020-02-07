184e3018ebd3f984370d5ec2d0c9faf3c90cf144
WoodsonWang <706469678@qq.com>
2020-02-07 16:16:38 +0800

    擦编写问题发布页

--- src/main/java/com/woodson/community/community/controller/PublishController.java
+++ src/main/java/com/woodson/community/community/controller/PublishController.java
@@ -1,12 +1,78 @@
 package com.woodson.community.community.controller;
 
+import com.woodson.community.community.mapper.QuestionMapper;
+import com.woodson.community.community.mapper.UserMapper;
+import com.woodson.community.community.model.Question;
+import com.woodson.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+    @Autowired(required = false)
+    private QuestionMapper questionMapper;
+    @Autowired(required = false)
+    private UserMapper userMapper;
+//    响应get请求
     @GetMapping("/publish")
-    public String Publish(){
+    public String publish(){
         return "publish";
+    }
+    //响应post请求
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam("title")String title,
+                            @RequestParam("description")String description,
+                            @RequestParam("tag")String tag,
+                            HttpServletRequest request,
+                            Model model){
+//        添加到model，当出错回到发布问题页面时，填充到页面中
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        if (title == null || title.equals("")){
+            model.addAttribute("error","问题不能为空");
+            return "publish";
+        }
+        if (description == null || description.equals("")){
+            model.addAttribute("error","描述不能为空");
+            return "publish";
+        }
+        if (tag == null || tag.equals("")){
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        if (user == null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        question.setCreator(user.getId());
+        questionMapper.insert(question);
+//        返回首页
+        return "redirect:/";
     }
 }
